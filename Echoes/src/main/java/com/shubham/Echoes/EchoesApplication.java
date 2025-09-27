cfbd6ca5d8b84438069eb6891af7c3d6186c80f2
Your Name <you@example.com>
2025-09-27 11:53:52 +0530

    setted upto use of annotation for managing transactions like transactional and enabetransactionmanagement and switched to mongodb atlas as necessary for transactions now next on to spring security

--- Echoes/src/main/java/com/shubham/Echoes/EchoesApplication.java
+++ Echoes/src/main/java/com/shubham/Echoes/EchoesApplication.java
@@ -2,12 +2,22 @@ package com.shubham.Echoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class EchoesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EchoesApplication.class, args);
 	}
+    @Bean
+    public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
+    }
 
 }
--- Echoes/src/main/java/com/shubham/Echoes/service/EchoesEntryService.java
+++ Echoes/src/main/java/com/shubham/Echoes/service/EchoesEntryService.java
@@ -6,6 +6,7 @@ import com.shubham.Echoes.repository.EchoesEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,13 +21,19 @@ public class EchoesEntryService {
     @Autowired
     private UserService userService;
 
-
+  @Transactional
     public void saveEntry(EchoesEntry echoesEntry,String userName){
-        User user = userService.findByUserName(userName);
-        echoesEntry.setDate(LocalDateTime.now());
-        EchoesEntry saved = echoesEntryRepository.save(echoesEntry);
-        user.getEchoesEntries().add(saved);
-        userService.saveUser(user);
+      try {
+
+
+          User user = userService.findByUserName(userName);
+          echoesEntry.setDate(LocalDateTime.now());
+          EchoesEntry saved = echoesEntryRepository.save(echoesEntry);
+          user.getEchoesEntries().add(saved);
+          userService.saveEntry(user);
+      }catch (Exception e) {
+        throw new RuntimeException("An error occurred while saving the entry.", e);
+    }
     }
     public void saveEntry(EchoesEntry echoesEntry){
 
