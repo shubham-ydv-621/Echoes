e7d4110d1aba22609305fe82e802059c16a117a4
Your Name <you@example.com>
2025-10-03 01:18:07 +0530

    added role based authorization like admin 22

--- Echoes/src/main/java/com/shubham/Echoes/service/UserService.java
+++ Echoes/src/main/java/com/shubham/Echoes/service/UserService.java
@@ -42,6 +42,13 @@ public class UserService {
         user.setRoles(Arrays.asList("USER"));
         userRepository.save(user);
     }
+
+    public void saveAdmin(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER", "ADMIN"));
+        userRepository.save(user);
+    }
+
     public void saveUser(User user) {
         userRepository.save(user);
     }
