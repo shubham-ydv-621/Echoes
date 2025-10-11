369d6cbb0bcd9e1ecf18c3f6e309e91e65571d63
Shubham-ydv-621 <shubham2006621@gmail.com>
2025-10-11 20:30:04 +0530

    Minimal Heroku build without MongoDB

--- src/main/java/com/shubham/Echoes/EchoesApplication.java
+++ src/main/java/com/shubham/Echoes/EchoesApplication.java
@@ -2,27 +2,29 @@ package com.shubham.Echoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+// Disable MongoDB auto-configuration so app can run without DB
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class
+        }
+)
 @EnableTransactionManagement
 @EnableScheduling
 public class EchoesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EchoesApplication.class, args);
-	}
-   /*
-@Bean
-public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
-    return new MongoTransactionManager(dbFactory);
-}
-*/
+    public static void main(String[] args) {
+        SpringApplication.run(EchoesApplication.class, args);
+    }
 
-
+    /*
+    // Commented out MongoDB transaction manager
+    @Bean
+    public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
+    }
+    */
 }
